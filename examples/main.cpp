abe53ec97dc052caccbaeaac1b9533fdfcf40002
Niyaz97 <niyaz160297@gmail.com>
2016-09-29 20:17:13 +0300

    Create main.cpp

---
+++ examples/main.cpp
@@ -0,0 +1,12 @@
+#include "../include/BinarySearchTree.h"
+
+
+int main() {
+
+    BinarySearchTree<int> tree{ 48,26,87,34,76};
+    tree.find(48);
+    tree.insert(29);
+    std::cout << tree;
+    file << tree;
+    
+}
