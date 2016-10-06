a41caf1e0e02b580d30b4a3745389a2b2f442914
Niyaz97 <niyaz160297@gmail.com>
2016-10-06 14:34:39 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,7 +4,7 @@ int main() {
 
     BinarySearchTree<int> tree1{ 52,34,87,34,76};
     BinarySearchTree<int> tree2=std::move(tree1);
- //   std::cout << tree1;
+    std::cout << tree1;
 //    std::ofstream file("/ClionProjects/untitled/Test1.txt");
 //    file << tree;
 //    file.close();
