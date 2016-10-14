9bb5a55fa9c7ee97e1379b99fb4ad7288081b3c6
Niyaz97 <niyaz160297@gmail.com>
2016-10-14 18:31:39 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,11 +5,19 @@ int main() {
 
     BinarySearchTree<int> tree1{ 52,34,87,76};
     BinarySearchTree<int> tree2=std::move(tree1);
- 
-    BinarySearchTree<int> treedel{100,250,400,50,150,600};
-    treedel.remove(treedel.root_,100);
-    std::cout<<treedel;
+    BinarySearchTree<int>   treedel{15,17,12,10,13,19,18,20};
+    BinarySearchTree<int> treecheck{15,17,10,13,19,18,20};
+    std::cout << tree1;
+    treedel.remove(12);
+    std::cout<<treedel<<std::endl;
+    std::cout<<treecheck<<std::endl;
 
+
+    BinarySearchTree<int>   treedel2{15,17,12,10,13,19,18,20};
+    BinarySearchTree<int> treecheck2{15,17,12,10,13,18,20};
+    treedel.remove(19);
+    std::cout<<treedel2<<std::endl;
+    std::cout<<treecheck2;
 //    std::ofstream file("/ClionProjects/untitled/Test1.txt");
 //    file << tree;
 //    file.close();
